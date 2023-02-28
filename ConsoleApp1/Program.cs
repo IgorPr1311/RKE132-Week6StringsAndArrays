1e7f95681bea0e6bc8025fd7454385ca228b7e08
Igor Pr <igor.prokofjev@tktk.ee>
2023-03-01 00:39:17 +0200

    add week6stringsandarrays code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿string hello = "hello, world1";
+int counter = 0;
+for (int i = 0;i < hello.Length; i++) 
+{
+    if (hello [i] == 'l') 
+    {
+        counter++;
+    }
+}
+Console.WriteLine($"There are {counter} l's in the string { hello}");
+//Random rnd = new Random();
+
+//int RandomColor = rnd.Next(0, colors.Length);
+
+//Console.WriteLine($"You should wear {colors[RandomColor]}");
